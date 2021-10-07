369275a25a87fd884383e531c205ce9c58943742
Kayla Golder <golderkayla@gmail.com>
2021-10-07 19:40:48 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// LOOPS
+
+// 1. create a loop that prints out the numbers 0 - 5 using a while loop
+
+// 2. create a loop that prints out the numbers 0 - 5 using a for loop
+
+// 3. create a loop that prints out the numbers 0 - 5 using a for loop but for each number return the squared version
+
+//4. iterate over the string below and print out each character
+let string = "the fox jumps over the moon"; 
+
